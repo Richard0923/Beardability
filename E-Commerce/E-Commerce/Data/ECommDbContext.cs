66763a717517ff45d3db1b728b26ebf16a0f219a
Jon Rice <revyolution1120@gmail.com>
2019-07-16 12:07:34 -0700

    Config setup

---
+++ E-Commerce/E-Commerce/Data/ECommDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data
+{
+    public class ECommDbContext: DbContext
+    {
+        public ECommDbContext(DbContextOptions<ECommDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
