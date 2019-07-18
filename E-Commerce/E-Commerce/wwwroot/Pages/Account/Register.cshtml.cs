8b22333ff2023c14675649ff4dccc152d24d646c
Meggan <meggtriplett@gmail.com>
2019-07-18 01:40:46 -0700

    Register.cshtml.cs user manager and sign in manager set. Should allow creation of users upon entries in register.cshtml. Using statements need to be added for models

--- E-Commerce/E-Commerce/wwwroot/Pages/Account/Register.cshtml.cs
+++ E-Commerce/E-Commerce/wwwroot/Pages/Account/Register.cshtml.cs
@@ -4,13 +4,64 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using E_Commerce.Models;
+
 
 namespace E_Commerce.wwwroot.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public RegisterModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    DOB = Input.DOB,
+                    PhoneNumber = Input.PhoneNumber
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Desciption);
+                    }
+                }
+
+                return Page();
+            }
         }
     }
 }
