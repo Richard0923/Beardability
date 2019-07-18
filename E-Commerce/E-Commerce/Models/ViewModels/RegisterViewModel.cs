973ebc812a5bb8886ea16cceb68fd42ead7aa33c
Meggan <meggtriplett@gmail.com>
2019-07-18 00:54:51 -0700

    Creating RegisterViewModel. Added DOB to ApplicationUser model

---
+++ E-Commerce/E-Commerce/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name ="Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+    }
+}
