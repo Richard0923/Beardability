187a17732865fca3494ac6c225a96c109d2a0370
Meggan <meggtriplett@gmail.com>
2019-07-31 10:48:40 -0700

    Fixing merge conflict with email receipt branch

--- E-Commerce/E-Commerce/Pages/Checkout/Receipt.cshtml.cs
+++ E-Commerce/E-Commerce/Pages/Checkout/Receipt.cshtml.cs
@@ -18,6 +18,11 @@ namespace E_Commerce.Pages.Checkout.Receipt
         private readonly IBasket _basket;
         private readonly IEmailSender _emailSender;
 
+        /// <summary>
+        /// Using instances of needed interfaces
+        /// </summary>
+        /// <param name="basket">Instance of the IBasket interface</param>
+        /// <param name="emailSender">Instance of the IEmailSender interface</param>
         public ReceiptModel(IBasket basket, IEmailSender emailSender)
         {
             _basket = basket;
@@ -25,6 +30,13 @@ namespace E_Commerce.Pages.Checkout.Receipt
         }
 
         public List<BasketItem> PurchaseItems { get; set; }
+
+        /// <summary>
+        /// Upon creation of the order receipt.
+        /// All purchased items are being displayed on receipt.
+        /// User will then receive an email to their registered email with a purchase receipt.
+        /// </summary>
+        /// <returns></returns>
         public async Task OnGet()
         {
             PurchaseItems = await _basket.GetAllBasketItems();
