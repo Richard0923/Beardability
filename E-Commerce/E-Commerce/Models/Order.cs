65665d77378a95a15bf7410e7f8df41de0263a20
richard0923 <r.rosadoocasio@gmail.com>
2019-08-07 20:38:10 -0700

    Added models for orders

---
+++ E-Commerce/E-Commerce/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public string ShipppingAddress { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ E-Commerce/E-Commerce/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int OrderID { get; set; }
+
+        public int Quanity { get; set; }
+
+        public int TotalItemPrice { get; set; }
+    }
+}
