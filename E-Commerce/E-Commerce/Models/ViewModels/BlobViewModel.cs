1fdbdd0dfffffa22093708936d21a5a6278f8945
Meggan Triplett <meggtriplett@gmail.com>
2019-08-09 03:51:14 -0700

    Blob View Model created

---
+++ E-Commerce/E-Commerce/Models/ViewModels/BlobViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models.ViewModels
+{
+    /// <summary>
+    /// View Model for Blob Images
+    /// </summary>
+    public class BlobViewModel
+    {
+       [Required]
+       public IFormFile Image { get; set; }
+    }
+}
