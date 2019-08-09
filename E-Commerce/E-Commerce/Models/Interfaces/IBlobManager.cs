30d3eb806e5f5d0b8348bf4cead9d916799f0d67
Meggan Triplett <meggtriplett@gmail.com>
2019-08-09 04:09:59 -0700

    Created iblobmanager which is interface for dynamically working with blob storage

---
+++ E-Commerce/E-Commerce/Models/Interfaces/IBlobManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models.Interfaces
+{
+    public interface IBlobManager
+	{
+        Task<CloudBlobContainer> GetContainer(string containerName);
+
+        Task<CloudBlob> GetBlob(string imageName, string containerName);
+
+        void UploadFile(CloudBlobContainer container, string fileName, string filePath);
+	}
+}
