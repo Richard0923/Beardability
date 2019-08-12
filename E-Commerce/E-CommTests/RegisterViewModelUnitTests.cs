7e6a5e342cda1dfe27474cb8da7ba2cf579f7275
Meggan Triplett <meggtriplett@gmail.com>
2019-08-11 20:31:41 -0700

    Completed get set tests for all properties in RegisterViewController

--- E-Commerce/E-CommTests/RegisterViewModelUnitTests.cs
+++ E-Commerce/E-CommTests/RegisterViewModelUnitTests.cs
@@ -79,5 +79,99 @@ namespace E_CommTests
             Assert.Equal("Weasley", registerView.LastName);
         }
 
+        [Fact]
+        public void CanGetDOB()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                DOB = new DateTime(07/31/1980)
+            };
+
+            Assert.Equal(new DateTime(07/31/1980), registerView.DOB);
+        }
+
+        [Fact]
+        public void CanSetDOB()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                DOB = new DateTime(07 / 31 / 1980)
+            };
+
+            registerView.DOB = new DateTime(07 / 24 / 1991);
+
+            Assert.Equal(new DateTime(07/24/1991), registerView.DOB);
+        }
+
+        [Fact]
+        public void CanGetPhoneNumber()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                PhoneNumber = "1234567890"
+            };
+
+            Assert.Equal("1234567890", registerView.PhoneNumber);
+        }
+
+        [Fact]
+        public void CanSetPhoneNumber()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                PhoneNumber = "1234567890"
+            };
+            registerView.PhoneNumber = "9876543210";
+
+            Assert.Equal("9876543210", registerView.PhoneNumber);
+        }
+
+        [Fact]
+        public void CanGetPassword()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                Password = "Test123$"
+            };
+
+            Assert.Equal("Test123$", registerView.Password);
+        }
+
+        [Fact]
+        public void CanSetPassword()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                Password = "Test123$"
+            };
+
+            registerView.Password = "newTest123$";
+
+            Assert.Equal("newTest123$", registerView.Password);
+        }
+
+        [Fact]
+        public void CanGetPasswordConfirmation()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                PasswordConfirmation = "Test123$"
+            };
+
+            Assert.Equal("Test123$", registerView.PasswordConfirmation);
+        }
+
+        [Fact]
+        public void CanSetPasswordConfirmation()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                PasswordConfirmation = "Test123$"
+            };
+
+            registerView.PasswordConfirmation = "newTest123$";
+
+            Assert.Equal("newTest123$", registerView.PasswordConfirmation);
+        }
     }
 }
