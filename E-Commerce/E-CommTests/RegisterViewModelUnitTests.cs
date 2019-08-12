80acb7e19c462e26a67a1a09f650b99ca86b7949
Meggan Triplett <meggtriplett@gmail.com>
2019-08-11 20:05:59 -0700

    Created file for RegisterViewModelUnitTests. Created passing get set test for email.

---
+++ E-Commerce/E-CommTests/RegisterViewModelUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using E_Commerce.Models.ViewModels;
+using System;
+using Xunit;
+
+
+namespace E_CommTests
+{
+    public class RegisterViewModelUnitTests
+    {
+        [Fact]
+        public void CanGetEmail()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                Email = "testemail@test.com"
+            };
+
+            Assert.Equal("testemail@test.com", registerView.Email);
+        }
+
+        [Fact]
+        public void CanSetEmail()
+        {
+            RegisterViewModel registerView = new RegisterViewModel()
+            {
+                Email = "testemail@test.com"
+            };
+
+            registerView.Email = "newtestemail@test.com";
+
+            Assert.Equal("newtestemail@test.com", registerView.Email);
+        }
+    }
+}
